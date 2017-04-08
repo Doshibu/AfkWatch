628d30ae8dc3981ae7c1f156c1695e6cd83feae2
Doshibu <guillaume.bonhommeau@viacesi.fr>
2017-04-08 02:53:27 +0200

    add LoadPays dataFixtures

---
+++ src/Doshibu/AfkWatchBundle/DataFixtures/ORM/LoadPays.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Doshibu\AfkWatchBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doshibu\AfkWatchBundle\Entity\Pays;
+
+class LoadPays implements FixtureInterface
+{
+  // Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
+	public function load(ObjectManager $manager)
+	{
+		// Liste des noms de catégorie à ajouter
+		$names = array(
+			'Allemagne',
+			'Argentine',
+			'Brésil',
+			'Chine',
+			'Etats-Unis',
+			'France',
+			'Inde',
+			'Indonésie',
+			'Italie',
+			'Japon',
+			'Mexique',
+			'Nigéria',
+			'Philippines',
+			'Royaume-Uni',
+			'Russie',
+			'Turquie'
+			);
+
+		foreach ($names as $name) 
+		{
+			// On crée la catégorie
+			$pays = new Pays();
+			$pays->setName($name);
+
+			// On la persiste
+			$manager->persist($pays);
+		}
+
+		// On déclenche l'enregistrement de toutes les catégories
+		$manager->flush();
+	}
+}
